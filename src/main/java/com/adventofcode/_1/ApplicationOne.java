98cbe7acd4db7b90c3620f655710b9854106f8c8
dkurnosenko <dimitri.kurnosenko@brain-child.de>
2022-12-03 00:42:23 +0100

    solved day 1.

---
+++ src/main/java/com/adventofcode/_1/ApplicationOne.java
@@ -0,0 +1,40 @@
+package com.adventofcode._1;
+
+import com.adventofcode.util.Const;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+import static java.util.Arrays.stream;
+
+public class ApplicationOne {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File(Const.RESOURCE_PATH + "input/01-1.txt"));
+        List<Integer> caloryList = new ArrayList<>();
+        Integer currentSum = 0;
+        while (scanner.hasNextLine()) {
+
+            String line = scanner.nextLine();
+            boolean isElveDone = Objects.equals(line, "");
+            if (isElveDone) {
+                caloryList.add(currentSum);
+                System.out.println("This Elve ate " + currentSum + " calories.");
+                currentSum = 0;
+            } else {
+                currentSum += Integer.parseInt(line);
+            }
+        }
+        System.out.println("Highest number is " + Collections.max(caloryList));
+        System.out.println("And now the sum 3 highest..");
+        Collections.sort(caloryList);
+        List<Integer> top3 = new ArrayList<Integer>(caloryList.subList(caloryList.size() -3, caloryList.size()));
+        Integer sumTop3 = top3.stream()
+                .mapToInt(a -> a)
+                .sum();
+
+        System.out.println(sumTop3);
+    }
+}
---
+++ src/main/java/com/adventofcode/util/Const.java
@@ -0,0 +1,6 @@
+package com.adventofcode.util;
+
+public class Const {
+    public static final String RESOURCE_PATH = "src/main/resources/";
+
+}
