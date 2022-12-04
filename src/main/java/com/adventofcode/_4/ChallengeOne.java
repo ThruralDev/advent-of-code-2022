ae369b675556805ff66dd4686ca61ffec66dd7cf
dkurnosenko <dimitri.kurnosenko@brain-child.de>
2022-12-04 21:39:52 +0100

    Basestructure and tasks for day 4.

--- src/main/java/com/adventofcode/_4/ChallengeOne.java
+++ src/main/java/com/adventofcode/_4/ChallengeOne.java
@@ -1,5 +1,32 @@
 package com.adventofcode._4;
 
+import com.adventofcode.util.Const;
+import com.adventofcode.util.Result;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ChallengeOne {
 
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File(Const.RESOURCE_PATH + "input/04.txt"));
+        Result result = new Result();
+
+        while (scanner.hasNextLine()) {
+
+            String line = scanner.nextLine();
+            System.out.println(line);
+
+            // TODO: 04.12.22 Take ranges into Strings. 
+
+            // TODO: 04.12.22 Check inclusion of one String with RegEx. 
+
+            // TODO: 04.12.22 Increment sum of result Regex matches.
+
+        }
+        // Reveal solution.
+        System.out.println(result.getSum());
+    }
 }
